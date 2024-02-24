662ee0bd9a0423f37fe3349b4cc254c20ee0f388
NjengaC <victorcyrus01@gmail.com>
2024-02-24 14:19:49 +0300

    comment

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,10 +1,9 @@
-#!/usr/bin/pup
 # Install flask (2.1.0)
 package {'flask':
   ensure => '2.1.0',
   provider => 'pip3'
 }
-
+# install werkzeug
 package {'werkzeug':
   ensure => '2.1.1',
   provider => 'pip3'
