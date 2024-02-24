7d289087fca5b617f4e3bb44575d6b5a77ebfe43
NjengaC <victorcyrus01@gmail.com>
2024-02-24 12:53:26 +0300

    mode

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,9 +1,8 @@
 #Creating a file in /tmp
 file { 'school':
   path    => '/tmp/school',
-  ensure  => file,
-  mode    => '0744',
   owner   => 'www-data',
   group   => 'www-data',
   content => 'I love Puppet',
+  mode    => '0744'
  }
