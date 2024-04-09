0006da6801906965e20bee0d21572f09942a902e
NjengaC <victorcyrus01@gmail.com>
2024-04-09 20:07:36 +0300

    results

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,23 +1,19 @@
 #!/usr/bin/python3
-"""
-Module returns the number of subscribers (not active users, total subscribers)
-for a given subreddit. If an invalid subreddit is given, the function returns 0
-"""
+"""returns the number of subscribers"""
+
 import requests
 
 
 def number_of_subscribers(subreddit):
-    """ Function returns a list of subscibers for a certain subreddit """
+    """Returns the number of subscribers"""
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-
     headers = {
         "User-Agent": "Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) \
         AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 \
             Mobile Safari/537.36"
     }
     response = requests.get(url, headers=headers, allow_redirects=False)
-
-    if response.status_code == 200 and 'data' in response.json():
-        return response.json()['data']['subscribers']
-    else:
+    if response.status_code == 404:
         return 0
+    results = response.json().get("data")
+    return results.get("subscribers")
